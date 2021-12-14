f105bca7722d9a8bd5079f1a11fc5ef369e0ae6e
Noora Peltokangas <noora.peltok@gmail.com>
2021-12-14 15:39:32 +0200

    Exercise 3.9 started

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -111,7 +113,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

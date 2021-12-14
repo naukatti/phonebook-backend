a736c23c3afc944e66f855777cef816ff8d78b19
Noora Peltokangas <noora.peltok@gmail.com>
2021-12-14 16:46:15 +0200

    Add build directory for frontend

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token("body", function (req, res) {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );

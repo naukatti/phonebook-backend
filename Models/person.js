89d58ac7b58f1be134a053196fabcd7b5f7f04b3
naukatti <noora.peltok@gmail.com>
2022-02-08 10:09:46 +0200

    phonenumber validation and a new build of frontend

--- Models/person.js
+++ Models/person.js
@@ -15,7 +15,16 @@ const personSchema = new mongoose.Schema({
     type: String,
     minlength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return v.length > 8 && /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
